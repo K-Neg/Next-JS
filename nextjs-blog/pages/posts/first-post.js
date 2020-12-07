8d324f07dcd4a8d3fb45fbb4c54e1773c0490c48
K-Neg <35573703+K-Neg@users.noreply.github.com>
2020-12-07 17:34:58 -0300

    add sorted blog props

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,14 +1,21 @@
+import Head from 'next/head'
 import Link from 'next/link'
+import Layout from '../../components/layout'
 
-export default function FirstPost(){
-    return( 
-        <>
-    <h1>AQUI</h1>
-    <h2>
-        <Link href="/">
-            <a>Back to home</a>
-        </Link>
-    </h2> 
-    </>
+export default function FirstPost() {
+    return (
+        <Layout>
+            <>
+                <Head>
+                    <title>TITULO</title>
+                </Head>
+                <h1>AQUI</h1>
+                <h2>
+                    <Link href="/">
+                        <a>Back to home</a>
+                    </Link>
+                </h2>
+            </>
+        </Layout>
     )
 }
